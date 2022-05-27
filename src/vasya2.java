b85933757ceefd2f9de8fa2dcf8ae1b483eb282b
supremah2 <supremah2@gmail.com>
2022-05-27 23:59:38 +0300

    новый класс вася

---
+++ src/vasya2.java
@@ -0,0 +1,12 @@
+public class vasya2 {
+
+    String name, surname;
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "vasya2{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
